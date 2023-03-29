ca905b223412a145fad7dd1ff69d94bf0c87988f
Lorena Castillejo <129109002+LorenaCastillejo@users.noreply.github.com>
2023-03-29 17:49:35 +0000

    made a new function

--- collab.c
+++ collab.c
@@ -25,7 +25,7 @@ void reed_ekidan2() {
 void amite2() {
   printf("Andrej Mitevski\n");
   printf("Thought that github was literally just a place to upload code, "
-         "learned it has a lot more than that\n");
+         "learned it has a lot more than that.\n");
 }
 
 void shadd3_alojko2() {
@@ -221,12 +221,6 @@ void dnazi2_lcasti39(){
   printf("Something that gave us trouble was figuring out the logistics of github, \n like creating repositories\n");
 }
 
-void ksabo4_heasam2(){
-  printf("Kyle Sabo\n");
-  printf("Harshavardhan Easam\n");
-  printf("We learned that you can directly connect a github repo to replit.\n");
-}
-
 int main() {
   printf(
       "This code has been created by the students of CS 211, Spring 2023!\n\n");
@@ -259,7 +253,6 @@ int main() {
   bpark40();
   ddizo2_tle94();
   dnazi2_lcasti39();
-  ksabo4_heasam2();
 
   return 0;
 }
