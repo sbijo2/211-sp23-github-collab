ca25d3adcbf5fdb211dbed87795aa46a0538487c
Wulong Situ <129301724+wsitu3@users.noreply.github.com>
2023-03-29 14:39:36 +0000

    changes

--- collab.c
+++ collab.c
@@ -24,13 +24,15 @@ void reed_ekidan2() {
 }
 void amite2() {
   printf("Andrej Mitevski\n");
-  printf("Thought that github was literally just a place to upload code, learned it has a lot more than that\n");
+  printf("Thought that github was literally just a place to upload code, "
+         "learned it has a lot more than that\n");
 }
 
 void shadd3_alojko2() {
   printf("Sammy Haddad and Angela Lojko\n");
-  printf("From the readings, we learned that git and GitHub are really effective when working on large projects with many people.\n");
-  printf("We can also see what a user changed/added to the program.\n"); 
+  printf("From the readings, we learned that git and GitHub are really "
+         "effective when working on large projects with many people.\n");
+  printf("We can also see what a user changed/added to the program.\n");
 }
 
 void ppacho2_grohr2() {
@@ -69,72 +71,82 @@ void stinu2_cdoma2() {
          "able to keep version and logs of a working tree/ code progress");
 }
 
-
 void jgongo2_pkoby2_kseki3() {
-    printf("Juli Gongora Falla, Phillip Kobylinski, and Kaito Sekiya\n");
-    printf("We learned that commit and push are seperate operations. "
-           "You can make multiple commmits before pushing to the repository.\n");
+  printf("Juli Gongora Falla, Phillip Kobylinski, and Kaito Sekiya\n");
+  printf("We learned that commit and push are seperate operations. "
+         "You can make multiple commmits before pushing to the repository.\n");
 }
 
-void pghimi3_ariver67(){
+void pghimi3_ariver67() {
   printf("Pratishat Ghimire and Arturo Rivera");
-  printf("We learned how to reflect the changes made locally, remotely on the original repo");
+  printf("We learned how to reflect the changes made locally, remotely on the "
+         "original repo");
 }
 
-void dzabi2(){
+void dzabi2() {
   printf("Denys Zabiyaka");
-  printf("I learned that Git allows developers to create multiple branches for their codebase.");
+  printf("I learned that Git allows developers to create multiple branches for "
+         "their codebase.");
 }
 
-void ddesle3_yshah33(){
-    printf("Dishant Desle, Yashwi Shah\n");
-    printf("We learned to connect and collaborate on a code under one\n"); 
-    printf("repository and that we can update the existinig repository.");
-    printf("We learned that github is online programming tool\n");
-    printf("that more software companies use to communicate about their project\n");
-    
+void ddesle3_yshah33() {
+  printf("Dishant Desle, Yashwi Shah\n");
+  printf("We learned to connect and collaborate on a code under one\n");
+  printf("repository and that we can update the existinig repository.");
+  printf("We learned that github is online programming tool\n");
+  printf(
+      "that more software companies use to communicate about their project\n");
 }
-    void jrosal21_melqa2(){
-    printf("Isaac Rosales, Musa Elqaq");
-    printf("Xcode has integrated source control in the toolbar which contains git hub commands.");
-    printf("Installing to local machine and keeping track of commits.");
+void jrosal21_melqa2() {
+  printf("Isaac Rosales, Musa Elqaq");
+  printf("Xcode has integrated source control in the toolbar which contains "
+         "git hub commands.");
+  printf("Installing to local machine and keeping track of commits.");
 }
 
-void vcolom2(){
+void vcolom2() {
   printf("Victor Colome\n");
-  printf("Learned that you can make changes to a repository in many different ways,\n");
+  printf("Learned that you can make changes to a repository in many different "
+         "ways,\n");
   printf("like with Git Bash, Git GUI, Git CMD, Replit, etc.\n");
 }
 
 void svoda_jbrya4() {
-printf("Svetlana Voda and Jakya Bryant\n");
-printf("From the readings, we learned that git and GitHub are  effective ways to store code in the cloud, in case there are issues with our local machines.\n");
-printf("We can monitor what changes users have made to the program.\n"); 
+  printf("Svetlana Voda and Jakya Bryant\n");
+  printf("From the readings, we learned that git and GitHub are  effective "
+         "ways to store code in the cloud, in case there are issues with our "
+         "local machines.\n");
+  printf("We can monitor what changes users have made to the program.\n");
   printf("We understand how to conect local repository with remote repository");
 }
 
-void dshah97_ysawa2(){
+void dshah97_ysawa2() {
   printf("Devam Shah and Yasmin Sawaf\n");
-  printf("From the readings we learned different command lines of git and github and how it helps us collaborate with many people at a time during a project.\n");
+  printf("From the readings we learned different command lines of git and "
+         "github and how it helps us collaborate with many people at a time "
+         "during a project.\n");
   printf("we also got to work on a collaborative project during a lab\n");
-  printf("we learned how cloning works and how local changes and global changes to the original vs the clone are different, this helps with twsting new ideas without effecting the original\n");
+  printf("we learned how cloning works and how local changes and global "
+         "changes to the original vs the clone are different, this helps with "
+         "twsting new ideas without effecting the original\n");
   printf("we understand how git works and we will work on it\n");
 }
 
 void rwisni2_ajoshi46() {
-    printf("Anshool Joshi, Remik Wisniewski");
-    printf("I learned how to collabrate with other on github");
-    printf("I learned new git commands");
-
+  printf("Anshool Joshi, Remik Wisniewski");
+  printf("I learned how to collabrate with other on github");
+  printf("I learned new git commands");
 }
 
 void aomar22() {
   printf("Ackchat Omar");
-  printf("I learned that git pull is combination of git fetch and git merge.\n");
+  printf(
+      "I learned that git pull is combination of git fetch and git merge.\n");
 }
 
 int main() {
-  printf("This code has been created by the students of CS 211, Spring 2023!\n\n");
+  printf(
+      "This code has been created by the students of CS 211, Spring 2023!\n\n");
   reed_ekidan2();
   amite2();
   shadd3_alojko2();
@@ -151,7 +163,7 @@ int main() {
   dshah97_ysawa2();
   svoda_jbrya4();
   rwisni2_ajoshi46();
-  
-    
+  // hey
+
   return 0;
 }
